ed233c0f408a7ae64a714fb945ee223051d109e9
HnimNart <minhtran1391@gmail.com>
2018-04-03 01:09:31 +0200

    Hello

--- src/Interpreter.fs
+++ src/Interpreter.fs
@@ -6,6 +6,7 @@ open AbSyn
 (* Exceptions for reporting run-time errors. *)
 exception MyError of string * Position
 exception MyError' of string * List<(string*int)>
+exception MyError'' of string * List<string*Position>
 
 type ProcTable = SymTab.SymTab<ProcDec>
 type VarTable = SymTab.SymTab<Value>
@@ -13,17 +14,49 @@ type VarTable = SymTab.SymTab<Value>
 let constZero = IntVal(0)
 
 // Checks if all variables in vSet are zero
+// Return a list of variables that are not zero
 let checkNonZeroVar(vTab, decl) =
     let isZero(x, vtab) =
       match SymTab.lookup x vtab with
         | Some (IntVal 0) -> []
         | Some (IntVal v) -> [(x,v)]
         | _               -> raise(MyError("Variable" + x + "not found", (0,0) ))
-
-
     List.fold (fun acc x -> acc @ isZero (x ,vTab)) [] decl
 
-// builds procedure symbol table
+// Checking macthing local and delocal statemetns
+// Returns a list of variables that do not have macthing
+// deallocations in a statement block
+let rec checkStatement(statList: Statement List,
+                       stack : List<string*Position>) =
+    match statList with
+        | [] -> stack
+        | s :: ss ->
+            match s with
+                | Local(var, e, pos) ->
+                    let stack' = (var, pos) :: stack
+                    checkStatement(ss, stack')
+                | Delocal(var, e, pos) ->
+                    let stack' = List.filter (fun (s, p) -> s <> var) stack
+                    checkStatement(ss, stack')
+                | If (e1, s1, s2, e2, pos) ->
+                    let stack1 = checkStatement(s1, [])
+                    if not stack1.IsEmpty then
+                       raise( MyError''("Unmatched local declaration in If", stack1))
+                    let stack2 = checkStatement(s2, [])
+                    if not stack2.IsEmpty then
+                       raise( MyError''("Unmatched local declaration in If", stack2))
+                    checkStatement(ss, stack)
+                | Repeat (s, e, pos) ->
+                    let stack' = checkStatement(s, [])
+                    if not stack'.IsEmpty then
+                       raise( MyError''("Unmatched local declaration in Repeat", stack'))
+                    checkStatement(ss, stack)
+
+                | _ ->  checkStatement(ss, stack)
+
+// Builds procedure symbol table
+// This is almost identical to the same function
+// from the IPS group project
 let rec buildPtab(pdecs: ProcDec list): ProcTable =
   match pdecs with
   | []              -> SymTab.empty()
@@ -33,7 +66,13 @@ let rec buildPtab(pdecs: ProcDec list): ProcTable =
     let pos   = getProcPos pdcl
     let ptab  = buildPtab ps
     match SymTab.lookup pid ptab with
-      | None         -> SymTab.bind pid pdcl ptab
+      | None         ->
+         // Check that every local statement has a corresponding delocal
+         let check = checkStatement(getProcStat pdcl, [])
+         if not check.IsEmpty then
+            raise( MyError''("Unmatched local declaration", check))
+         SymTab.bind pid pdcl ptab
+
       | Some ofdecl  ->
           (* Report the first occurrence of the name. *)
           raise (MyError ("Already defined procedure : "+pid, getProcPos ofdecl))
@@ -43,25 +82,25 @@ let rec evalStatement(s: Statement,
                       ptab: ProcTable,
                       gSet: string list): VarTable =
   match s with
-  | PlusAssignment(id, e, pos)  ->
-      let eVal = evalExp(e, vtab, id)
-      let value = SymTab.lookup id vtab
+  | PlusAssignment(var, e, pos)  ->
+      let eVal = evalExp(e, vtab, var)
+      let value = SymTab.lookup var vtab
       match value with
         | None  ->
-            raise (MyError("LHS variable " + id + " is not defined", pos))
+            raise (MyError("LHS variable " + var + " is not defined", pos))
         | Some n  ->
             let newVal = evalExp (Plus(Constant(n, pos), Constant(eVal,pos),pos), vtab , "")
-            SymTab.bind id newVal vtab
+            SymTab.bind var newVal vtab
 
-  | MinusAssignment(id, e, pos )  ->
-      let eVal = evalExp(e, vtab, id)
-      let value = SymTab.lookup id vtab
+  | MinusAssignment(var , e, pos )  ->
+      let eVal = evalExp(e, vtab, var)
+      let value = SymTab.lookup var vtab
       match value with
         | None  ->
-            raise (MyError("LHS variable " + id + " is not defined", pos))
+            raise (MyError("LHS variable " + var + " is not defined", pos))
         | Some n  ->
             let newVal = evalExp (Minus(Constant(n, pos), Constant(eVal,pos),pos), vtab, "")
-            SymTab.bind id newVal vtab
+            SymTab.bind var newVal vtab
 
   | If(e1, s1, s2, e2, pos)  ->
       let (vtab', cond) =
@@ -89,18 +128,18 @@ let rec evalStatement(s: Statement,
         | IntVal v  ->
           if v = 0
           then
-              raise ( MyError("Repeat: Condition false at entry", pos))
+              raise (MyError("Repeat: Condition false at entry", pos))
           else
               repeatEval(s1, e1, vtab, ptab, gSet)
 
   | Call (id, param, pos)  ->
       match SymTab.lookup id ptab with
-        | None    -> raise( MyError("Undefined procedure: " + id, pos))
+        | None    -> raise( MyError("Call to undefined procedure: " + id, pos))
         | Some p  -> callProcWithVtable (p, param, vtab, ptab, pos, gSet)
 
   | Uncall (id, param, pos)  ->
       match SymTab.lookup id ptab with
-        | None    -> raise( MyError("Undefined procedure: " + id, pos))
+        | None    -> raise( MyError("Uncall to undefined procedure: " + id, pos))
         | Some (ProcDec(id, parg, stat, pos))  ->
             let iStat = ReverseProg.inverseStatementList (ReverseProg.rev stat [])
             callProcWithVtable (ProcDec(id, parg, iStat, pos), param , vtab, ptab, pos, gSet)
@@ -128,16 +167,16 @@ let rec evalStatement(s: Statement,
                    let value : int  = int(Console.ReadLine())
                    let v' = IntVal(value)
                    SymTab.bind var v' vtab
-         | None -> raise(MyError("Argument "+var+" in read not defined", pos))
+         | None -> raise( MyError("Argument "+var+" in read not defined", pos))
 
    | Local(var, e, pos) ->
        match SymTab.lookup var vtab with
            | None ->  let e' = evalExp(e, vtab, var)
                       SymTab.bind var e' vtab
            | Some v ->
-                      raise(MyError("Variable " + var + "is already defined", pos))
+                      raise(MyError("Variable " + var + " is already defined", pos))
 
-   | Delocal (var, e, pos) ->
+   | Delocal(var, e, pos) ->
        SymTab.remove var vtab
 
    | Swap(var1, var2, pos) ->
@@ -230,7 +269,17 @@ and evalExp(e: Exp,
               match (n1, n2) with
                   | (x,y) when x <> 0 && y <> 0 -> IntVal(1)
                   | _                           -> IntVal(0)
+  | Not (e, pos)  ->
+        let r1 = evalExp(e, vtab , lhsVar)
+        match r1 with
+          | (IntVal n1)  ->
+              match n1 with
+                  | x when x = 0 -> IntVal(1)
+                  | _            -> IntVal(1)
 
+
+// A function for Repeat statement after
+// the initial test that the condition is true
 and repeatEval(statList: Statement List,
                e : Exp,
                vtab : VarTable,
@@ -246,34 +295,39 @@ and repeatEval(statList: Statement List,
       else
           repeatEval(statList, e, vtab', ptab, gSet)
 
+// Executes a procedure call
 and callProcWithVtable (procdec: ProcDec,
                         aargs : Param List,
                         vtab  : VarTable,
                         ptab  : ProcTable,
                         poscall : Position,
                         gSet : string List) =
-    let (ProcDec (pid, pargs,  statements, position)) = procdec
+
+    let (ProcDec (pid, pargs, statements, position)) = procdec
 
     // Check if arg lists match
     if pargs.Length <> aargs.Length then
         raise(MyError("Wrong number of args", poscall))
 
     let aargsStr = List.map (fun x -> getStringOfParam x) aargs
-    let VarToPassed = Set.toList(Set.union (Set.ofList gSet) (Set.ofList aargsStr))
-    // Only get the values from the global list and
+    let passedVars = Set.toList(Set.union (Set.ofList gSet) (Set.ofList aargsStr))
+
+    // Only the values from the global list and
     // the local variables the are passed in to procedure
     let vtab' = List.fold (fun acc x ->
                            match SymTab.lookup x vtab with
-                           // None should never happen
-                           | None   -> raise(MyError("Variable not found in SymbolTable", poscall))
-                           | Some v -> SymTab.bind x v acc ) (SymTab.empty()) VarToPassed
+                             | None   ->  // None should never happen
+                                raise(MyError("Variable not found in SymbolTable", poscall))
+                             | Some v ->
+                                SymTab.bind x v acc ) (SymTab.empty()) passedVars
 
     // Creates a list of pairs of actual and formal parameters
     let varPair  = List.zip pargs aargs
     // Substitute formal parameters in body with actual
-    let s' = Substitute.subsVarsInSList(statements, varPair)
+    let s' = Substitute.subStatementList(statements, varPair)
     evalStatementList(s', vtab', ptab, gSet)
 
+// Recursively evaluates a list of statements
 and evalStatementList(sList : Statement List,
                       vtab : VarTable,
                       ptab : ProcTable,
@@ -284,22 +338,28 @@ and evalStatementList(sList : Statement List,
             let vtab' = evalStatement(s, vtab, ptab,gSet)
             evalStatementList(ss, vtab', ptab, gSet)
 
+// Evaluates a whole program (The main of the interpreter)
 and evalProg (prog: Program) : int =
 
     let (decl, statements, proc) = prog
     let ptab = buildPtab (proc)
     let declStr = List.map (fun x -> getStringOfDecl x) decl
     // Build init symbol table with zeroes from list of 'globals'
-    let vTab = List.fold (fun acc x -> SymTab.bind x constZero acc) (SymTab.empty()) declStr
+    let vtab = List.fold (fun acc x -> SymTab.bind x constZero acc) (SymTab.empty()) declStr
+
+    // Check that every local statement has a corresponding delocal
+    let check = checkStatement(statements, [])
+    if not check.IsEmpty then
+        raise( MyError''("Unmatched local declaration", check))
 
     if not (declStr = List.distinct declStr) then
         raise (MyError("Dublicate variables in declaration", (0,0)))
 
     // Run 'main' statements
-    let vTab' = evalStatementList (statements, vTab, ptab, declStr)
+    let vtab' = evalStatementList (statements, vtab, ptab, declStr)
 
     // Check if all variables are zero
-    let endVals = checkNonZeroVar(vTab', declStr)
+    let endVals = checkNonZeroVar(vtab', declStr)
     if List.isEmpty endVals then
         0
     else
